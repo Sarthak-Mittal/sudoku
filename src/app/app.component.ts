bafab7994d17d19f67fc1e76e4e6722cd3aa0293
Sarthak-Mittal <sarthakmittal20@gmail.com>
2020-03-07 18:07:00 +0530

    modify syntax for efficiency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,82 @@
-import { Component } from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
+// import {GridData} from './utils/GridData'
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-
-  name = 'Sudoku';
+export class AppComponent implements OnInit {
+  name = "Sudoku";
   x = "";
   y = "";
+
   constructor(private modalService: NgbModal) {}
 
-  
-  recieveValue($event){
-    this.data[this.x][this.y].value = $event;
+  ngOnInit() {
+
   }
 
-   open( x, y, comp) {
-     this.x = x;
-     this.y = y;
-    this.modalService.open(comp, { centered: true , size: 'sm' });
+  recieveValue($event) {
+    this.data[this.x][this.y].v = $event;
   }
+
+  open(x, y, comp) {
+    this.x = x;
+    this.y = y;
+    this.modalService.open(comp, { centered: true, size: "sm" });
+  }
+
+
 
 
   data = [
-  [ 
-    {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-    {"value":1, "clickable": true}, {"value":5, "clickable": false},{"value":6, "clickable": true},
-    {"value":1, "clickable": false}, {"value":8, "clickable": true},{"value":9, "clickable": false}
-  ],
-  [
-      {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-      {"value":4, "clickable": false}, {"value":5, "clickable": true},{"value":6, "clickable": true},
-      {"value":7, "clickable": true}, {"value":8, "clickable": true},{"value":9, "clickable": false}
-  ],
-  [ 
-    {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-    {"value":4, "clickable": false}, {"value":5, "clickable": true},{"value":6, "clickable": true},
-    {"value":7, "clickable": true}, {"value":8, "clickable": true},{"value":9, "clickable": true}
-  ],
-  [
-      {"value":1, "clickable": false}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-      {"value":4, "clickable": true}, {"value":5, "clickable": false},{"value":6, "clickable": true},
-      {"value":7, "clickable": true}, {"value":8, "clickable": true},{"value":9, "clickable": false}
-  ],
-  [ 
-    {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-    {"value":4, "clickable": true}, {"value":5, "clickable": true},{"value":6, "clickable": false},
-    {"value":7, "clickable": false}, {"value":8, "clickable": false},{"value":9, "clickable": true}
-  ],
-  [
-      {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-      {"value":4, "clickable": false}, {"value":5, "clickable": true},{"value":6, "clickable": false},
-      {"value":7, "clickable": true}, {"value":8, "clickable": false},{"value":9, "clickable": true}
-  ],
-  [ 
-    {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-    {"value":4, "clickable": true}, {"value":5, "clickable": true},{"value":6, "clickable": true},
-    {"value":7, "clickable": true}, {"value":8, "clickable": true},{"value":9, "clickable": true}
-  ],
-  [
-      {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-      {"value":4, "clickable": true}, {"value":5, "clickable": true},{"value":6, "clickable": true},
-      {"value":7, "clickable": true}, {"value":8, "clickable": true},{"value":9, "clickable": true}
-  ],
-  [ 
-    {"value":1, "clickable": true}, {"value":2, "clickable": false},{"value":3, "clickable": true},
-    {"value":4, "clickable": false}, {"value":5, "clickable": false},{"value":6, "clickable": true},
-    {"value":7, "clickable": true}, {"value":8, "clickable": true},{"value":9, "clickable": false}
-  ]
-];
-
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 1, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 1, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ],
+    [
+      { v: 1, s: true },{ v: 2, s: true },{ v: 3, s: true },
+      { v: 4, s: true },{ v: 5, s: true },{ v: 6, s: true },
+      { v: 7, s: true },{ v: 8, s: true },{ v: 9, s: true }
+    ]
+  ];
 }
